ec6b8edc601d3c1bfa878dc69e7ab5cdde82a283
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2023-01-28 12:54:10 +0200

    update

--- app/Http/Controllers/SuperAdmin_functions.php
+++ app/Http/Controllers/SuperAdmin_functions.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
-class user_functions extends Controller
+class SuperAdmin_functions extends Controller
 {
     public function dashboard()
     {
